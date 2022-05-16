77f1db5dd197fa997a2c9e8c5f56aa68b7462fb4
anthonyztan <rztan@up.edu.ph>
2022-05-16 11:53:53 +0800

    all files

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div>
+  <h1>STORE</h1>
+  <div class = "row">
+    <div class = "column" v-for = "product in productList" :key = "product.name">
+      <product-card :item="product"></product-card>
+    </div>
+  </div>    
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductCard from './components/ProductCard.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    ProductCard
+  },
+  data () {
+    return {
+      productList: [
+        {name: 'Glasses', price: '24.99', desc: 'To see better'},
+        {name: 'Mouse', price: '20.00', desc: 'Computer mouse'}
+      ]
+    }
+  },
 }
 </script>
 
@@ -22,5 +36,16 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.column {
+  float: left;
+  width: 50%;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
 }
 </style>
